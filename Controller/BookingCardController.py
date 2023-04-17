e43b8812f65840819d10051764cf5272e0657f42
Дмитрий Сазонов <sazonov.di@students.dvfu.ru>
2023-04-17 12:41:44 +1000

    Дата бронирования, в нужном формате, сразу передается в параметры метода booking

--- Controller/BookingCardController.py
+++ Controller/BookingCardController.py
@@ -5,20 +5,25 @@ import Model.TablesModel as tables
 import Model.UsersModel as users
 
 
-def booking(date, cache):
+def booking(bookingDate, cache):
     allTime = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
                '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
 
     if request.method == "POST":
-        users.addGuest(request.form['fio'], request.form['phoneNumber'])
-        guestID = users.getGuest(request.form['fio'], request.form['phoneNumber'])
-        placeID = tables.getTableLocation(int(request.form['floor']), request.form['window'])
-        tableID = tables.getFreeTable(int(request.form['countPlaces']), placeID)
-
-        bookingDate = str(date[-4:]) + "-" + str(date[2:4]) + "-" + str(date[0:2])
         beginBookingTime = request.form['beginBookingTime']
         endBookingTime = request.form['endBookingTime']
         countPlaces = int(request.form['countPlaces'])
+
+        users.addGuest(request.form['fio'], request.form['phoneNumber'])
+        guestID = users.getGuest(request.form['fio'], request.form['phoneNumber'])
+        placeID = tables.getTableLocation(int(request.form['floor']), request.form['window'])
+        tableID = tables.getFreeTable(
+            int(request.form['countPlaces']),
+            placeID,
+            bookingDate,
+            beginBookingTime,
+            endBookingTime
+        )
 
         bookingModel.addBooking(tableID, guestID, bookingDate, beginBookingTime, endBookingTime,  countPlaces)
         cache.set('booking_true', 'True')
