011b9f446e1df62a62b0e186cb5f1533ec193c29
Дмитрий Сазонов <sazonov.di@students.dvfu.ru>
2023-04-19 10:19:20 +1000

    Добавление редактора на страницу с информацией о всех бронированиях.

--- Controller/AdministrationController.py
+++ Controller/AdministrationController.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, request, url_for
 
 import Model.BookingModel as booking
 
@@ -7,4 +7,26 @@ def allBooking(cache):
     if cache.get('login') is None:
         return redirect(url_for('login'))
     else:
-        return render_template('AdministrationTemplate.html', allBooking=booking.getAllBooking())
+        clickButtonEdit = None
+        clickButtonDelete = None
+
+        if request.method == "POST":
+            if 'buttonEdit' in request.form:
+                clickButtonEdit = request.form['buttonEdit']
+            elif 'buttonDelete' in request.form:
+                clickButtonDelete = request.form['buttonDelete']
+                bookingDate = str(request.form['bookingDate'])
+                beginTime = str(request.form['beginTime'])
+                tableNumber = int(request.form['tableNumber'])
+
+                booking.removeBooking(bookingDate, beginTime, tableNumber)
+
+        return render_template(
+            'AdministrationTemplate.html',
+            allBooking=booking.getAllBooking(),
+            clickButtonEdit=clickButtonEdit,
+            clickButtonDelete=clickButtonDelete,
+            bookingDates=booking.getDates(),
+            bookingBeginTime=booking.getBeginTimes(),
+            bookingTableNumbers=booking.getTableNumbers()
+        )
