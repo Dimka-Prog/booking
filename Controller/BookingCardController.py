28aa58591b957ae8f147211e04dbfc21296f78da
Дмитрий Сазонов <sazonov.di@students.dvfu.ru>
2023-04-19 22:44:22 +1000

    Добавлены параметры в метод booking и сделана проверка на авторизацию для пользователя

--- Controller/BookingCardController.py
+++ Controller/BookingCardController.py
@@ -49,7 +49,10 @@ def getFreeDate():
     return result_close
 
 
-def booking():
+def booking(guestID, cache):
+    if cache.get('user') is None:
+        return redirect(url_for('login'))
+
     allTime = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
                '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
 
@@ -62,8 +65,7 @@ def booking():
         endBookingTime = request.form['endBookingTime']
         countPlaces = int(request.form['countPlaces'])
 
-        users.addGuest(request.form['fio'], request.form['phoneNumber'])
-        guestID = users.getGuest(request.form['fio'], request.form['phoneNumber'])
+        users.updateGuest(guestID, request.form['fio'], request.form['phoneNumber'])
         placeID = tables.getTableLocation(int(request.form['floor']), request.form['window'])
         tableID = tables.getFreeTable(
             int(request.form['countPlaces']),
