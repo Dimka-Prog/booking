94447eef4fb67188ed82406a047b7bf60afee9f7
Дмитрий Сазонов <sazonov.di@students.dvfu.ru>
2023-04-19 22:40:32 +1000

    Полная переработка запросов к таблицам Сотрудников и Гостей.

--- Model/UsersModel.py
+++ Model/UsersModel.py
@@ -1,25 +1,55 @@
 import Database.connectDB as db
 
 
-def addGuest(fio, phoneNumber):
+def updateGuest(guestID, fio, phoneNumber):
     connectDB = db.getConnection()
     cursor = connectDB.cursor()
 
     cursor.execute(f'''
-                    INSERT INTO Guests (FIO, PhoneNumber) 
-                    VALUES ('{fio}', '{phoneNumber}')
+                    UPDATE Guests
+                    SET FIO = '{fio}', PhoneNumber = '{phoneNumber}'
+                    WHERE GuestID = {guestID}
                     ''')
     connectDB.commit()
 
 
-def getGuest(fio, phoneNumber):
+def addGuest(login, password):
     connectDB = db.getConnection()
     cursor = connectDB.cursor()
 
-    guestID = cursor.execute(f'''
-                              SELECT GuestID 
+    cursor.execute(f'''
+                    INSERT INTO Guests (Login, Password) VALUES 
+                    ('{login}', '{password}')
+                    ''')
+    connectDB.commit()
+
+
+def getAuthGuest(login, password):
+    connectDB = db.getConnection()
+    cursor = connectDB.cursor()
+
+    authGuest = cursor.execute(f'''
+                              SELECT
+                                    GuestID,
+                                    Login,
+                                    Password 
                               FROM Guests 
-                              WHERE FIO = '{fio}' AND PhoneNumber = '{phoneNumber}'
+                              WHERE Login = '{login}' AND Password = '{password}'
                              ''').fetchone()
     connectDB.close()
-    return guestID[0]
+    return authGuest
+
+
+def getAuthStaff(login, password):
+    connectDB = db.getConnection()
+    cursor = connectDB.cursor()
+
+    authStaff = cursor.execute(f'''
+                              SELECT
+                                    Login,
+                                    Password 
+                              FROM Staff 
+                              WHERE Login = '{login}' AND Password = '{password}'
+                             ''').fetchone()
+    connectDB.close()
+    return authStaff
